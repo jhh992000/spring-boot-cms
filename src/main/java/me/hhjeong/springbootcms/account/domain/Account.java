c80c439b2a43bba0f13166dab181ff4445d9af4b
HoonHee Jeong <jhh992000@gmail.com>
2021-09-24 18:49:19 +0900

    refactor: accountRole fk, fk명 부여

--- src/main/java/me/hhjeong/springbootcms/account/domain/Account.java
+++ src/main/java/me/hhjeong/springbootcms/account/domain/Account.java
@@ -1,14 +1,17 @@
 package me.hhjeong.springbootcms.account.domain;
 
 import java.util.Set;
+import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import me.hhjeong.springbootcms.common.domain.BaseEntity;
 
 @Entity
@@ -23,6 +26,7 @@ public class Account extends BaseEntity {
     private String password;
 
     @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "account_roles", joinColumns = @JoinColumn(name = "account_id"), foreignKey = @ForeignKey(name = "fk_account_role"))
     @Enumerated(EnumType.STRING)
     private Set<AccountRole> roles;
 
