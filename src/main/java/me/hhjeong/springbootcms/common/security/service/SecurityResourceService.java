bd5f27255b1e15c94707d98f560376bcfb757629
HoonHee Jeong <jhh992000@gmail.com>
2021-10-06 03:33:01 +0900

    refactor: 소스정리 및 @Transactional 이동
    - JPA에서 LazyLoading은 @Transactional 범위내에서 가능하다는 점 유의할것

--- src/main/java/me/hhjeong/springbootcms/common/security/service/SecurityResourceService.java
+++ src/main/java/me/hhjeong/springbootcms/common/security/service/SecurityResourceService.java
@@ -15,7 +15,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Service;
 
-@Transactional
 @Service
 public class SecurityResourceService {
 
@@ -29,21 +28,26 @@ public class SecurityResourceService {
         this.resourcesRepository = resourcesRepository;
     }
 
-    public LinkedHashMap<RequestMatcher, List<ConfigAttribute>> getResourceList(){
+    /**
+     * DB에서 권한과 자원정보를 가져와서 맵으로 반환한다.
+     * {요청URL:[권한리스트]}
+     *
+     * @return
+     */
+    @Transactional
+    public LinkedHashMap<RequestMatcher, List<ConfigAttribute>> getResourceList() {
+        List<Resources> resourcesList = resourcesRepository.findAll();
+        List<RoleResources> roleResources = roleResourcesRepository.findAll();
 
-        // DB로부터 권한과 자원정보를 가져온다.
-        LinkedHashMap<RequestMatcher,List<ConfigAttribute>> result = new LinkedHashMap<>();
-        List<Resources> resourcesList = resourcesRepository.findAll();  // Resources 데이터 전부 가져오기
-        List<RoleResources> roleResources = roleResourcesRepository.findAll();  // RoleResources 데이터 전부 가져오기
-        resourcesList.forEach(resource ->{  // For문 돌면서~
-            List<ConfigAttribute> configAttributeList = new ArrayList<>();  // 권한정보 저장 리스트 생성해주고 여기에 담을꺼다.
-            // 자원에 해당되는 권한 목록을 가져온다.
-            Long resourceId = resource.getId();   // for문 내부에서 도는 요소. resourceId.
-            roleResources.forEach(roleResource ->{  // 이중 For문 시작!
-                if(roleResource.getResources().getId() == resourceId)   // Resource의 아이디와 roleResource에서 뽑은 아이디가 같다면
-                    configAttributeList.add(new SecurityConfig(roleResource.getRole().getRoleName()));  // 권한정보 이름 더하기!
+        LinkedHashMap<RequestMatcher, List<ConfigAttribute>> result = new LinkedHashMap<>();
+        resourcesList.forEach(resource -> {
+            List<ConfigAttribute> configAttributeList = new ArrayList<>();
+            roleResources.forEach(roleResource -> {
+                if (roleResource.getResources().getId() == resource.getId()) {
+                    configAttributeList.add(new SecurityConfig(roleResource.getRole().getRoleName()));
+                }
             });
-            result.put(new AntPathRequestMatcher(resource.getResourceName()),configAttributeList);  // 해시로 {이름:[권한들]} 매핑해주기
+            result.put(new AntPathRequestMatcher(resource.getResourceName()), configAttributeList);
         });
         return result;
     }
