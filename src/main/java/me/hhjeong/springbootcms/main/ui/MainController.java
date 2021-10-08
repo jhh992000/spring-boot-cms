78e2b30397349e569a95d98eba260e97c2d8abf1
HoonHee Jeong <jhh992000@gmail.com>
2021-10-09 08:25:59 +0900

    refactor: 메인컨트롤러 패키지변경

--- src/main/java/me/hhjeong/springbootcms/main/ui/MainController.java
+++ src/main/java/me/hhjeong/springbootcms/main/ui/MainController.java
@@ -1,4 +1,4 @@
-package me.hhjeong.springbootcms.controller;
+package me.hhjeong.springbootcms.main.ui;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
