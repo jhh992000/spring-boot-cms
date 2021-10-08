31f096b69bc162ec3b54d410627a98da639769d7
HoonHee Jeong <jhh992000@gmail.com>
2021-10-09 06:43:45 +0900

    refactor: dto 에 롬복사용

--- src/main/java/me/hhjeong/springbootcms/account/dto/AccountResponse.java
+++ src/main/java/me/hhjeong/springbootcms/account/dto/AccountResponse.java
@@ -1,26 +1,19 @@
 package me.hhjeong.springbootcms.account.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import me.hhjeong.springbootcms.account.domain.Account;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountResponse {
 
     private Long id;
     private String usename;
 
-    public AccountResponse(Long id, String usename) {
-        this.id = id;
-        this.usename = usename;
-    }
-
     public static AccountResponse of(Account account) {
         return new AccountResponse(account.getId(), account.getUsername());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getUsename() {
-        return usename;
     }
 }
