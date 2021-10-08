57aabf679d69a95dc804713b28c00a6f575e758d
HoonHee Jeong <jhh992000@gmail.com>
2021-10-09 08:26:31 +0900

    feat: refresh 토큰 갱신용 컨트롤러 추가

--- src/main/java/me/hhjeong/springbootcms/common/security/controller/JwtController.java
+++ src/main/java/me/hhjeong/springbootcms/common/security/controller/JwtController.java
@@ -1,41 +1,60 @@
 package me.hhjeong.springbootcms.common.security.controller;
 
+import static me.hhjeong.springbootcms.common.security.handler.CustomAuthenticationSuccessHandler.REFRESH_TOKEN_PREFIX;
+
 import me.hhjeong.springbootcms.common.response.ApiException;
 import me.hhjeong.springbootcms.common.response.ExceptionEnum;
-import me.hhjeong.springbootcms.common.security.dto.TokenDto;
+import me.hhjeong.springbootcms.common.security.dto.RefreshTokenRequest;
+import me.hhjeong.springbootcms.common.security.dto.TokenResponse;
 import me.hhjeong.springbootcms.common.security.jwt.TokenProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JwtController {
 
+    private static final Logger logger = LoggerFactory.getLogger(JwtController.class);
+
     @Autowired
     private TokenProvider tokenProvider;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
-    @PostMapping("/api/v1/refreshToken")
-    public ResponseEntity refreshToken(@RequestParam("token") String token) {
-        if (!tokenProvider.validateToken(token)) {
+    @PostMapping("/api/refreshToken")
+    public ResponseEntity refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+        String clientRefreshToken = refreshTokenRequest.getRefreshToken();
+
+        //refreshToken 1차 검증
+        if (!tokenProvider.validateToken(clientRefreshToken)) {
             throw new ApiException(ExceptionEnum.SECURITY_02);
         }
 
-        //TODO - refreshToken 서버에서 한번 더 검증 (redis)
-
-        Authentication authentication = tokenProvider.getAuthentication(token);
+        Authentication authentication = tokenProvider.getAuthentication(clientRefreshToken);
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        String key = REFRESH_TOKEN_PREFIX + userDetails.getUsername();
+        String savedRefreshToken = redisTemplate.opsForValue().get(key);
+
+        //refreshToken 2차 검증 (redis에 저장된 토큰)
+        if (!tokenProvider.validateToken(savedRefreshToken)) {
+            throw new ApiException(ExceptionEnum.SECURITY_02);
+        }
 
         String accessToken = tokenProvider.createToken(userDetails);
         String refreshToken = tokenProvider.createRefreshToken(userDetails);
 
-        //TODO - refreshToken 서버에 저장 (to redis)
+        TokenResponse tokenResponse = new TokenResponse(accessToken, refreshToken);
+        logger.debug("tokenDto : {}", tokenResponse);
 
-        TokenDto tokenDto = new TokenDto(accessToken, refreshToken);
-        return ResponseEntity.ok().body(tokenDto);
+        return ResponseEntity.ok().body(tokenResponse);
     }
 
 }
