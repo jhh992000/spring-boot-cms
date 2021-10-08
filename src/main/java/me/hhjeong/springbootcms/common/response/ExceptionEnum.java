f265c58efc101af66f01060f7139815c24266912
HoonHee Jeong <jhh992000@gmail.com>
2021-10-08 18:48:20 +0900

    feat: 공통 예외처리 클래스 추가

---
+++ src/main/java/me/hhjeong/springbootcms/common/response/ExceptionEnum.java
@@ -0,0 +1,31 @@
+package me.hhjeong.springbootcms.common.response;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@ToString
+public enum ExceptionEnum {
+    RUNTIME_EXCEPTION(HttpStatus.BAD_REQUEST, "E0001"),
+    ACCESS_DENIED_EXCEPTION(HttpStatus.UNAUTHORIZED, "E0002"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E0003"),
+
+    SECURITY_01(HttpStatus.UNAUTHORIZED, "S0001", "권한이 없습니다."),
+    SECURITY_02(HttpStatus.UNAUTHORIZED, "S0002", "토큰이 만료되었습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private String message;
+
+    ExceptionEnum(HttpStatus status, String code) {
+        this.status = status;
+        this.code = code;
+    }
+
+    ExceptionEnum(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}
