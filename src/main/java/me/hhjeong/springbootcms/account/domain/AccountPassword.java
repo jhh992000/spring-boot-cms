3732851e70f538f4ce8399a4586f3201a4e0fa34
HoonHee Jeong <jhh992000@gmail.com>
2021-10-16 02:35:55 +0900

    feat: 커스텀 annotation 필수정의요소 추가
    1. message : 메시지 관리
    2. groups : 상황별 validation 제어
    3. payload : 심각도

--- src/main/java/me/hhjeong/springbootcms/account/domain/AccountPassword.java
+++ src/main/java/me/hhjeong/springbootcms/account/domain/AccountPassword.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
@@ -12,4 +13,6 @@ import javax.validation.Constraint;
 public @interface AccountPassword {
 
     String message() default "비밀번호가 유효하지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
