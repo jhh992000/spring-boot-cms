5a986d3ee5e4245aa0bbb71abecb52d925c17bf2
HoonHee Jeong <jhh992000@gmail.com>
2021-10-16 02:38:00 +0900

    feat: 계정생성 DTO에 커스텀 유효성검증 애노테이션 추가

--- src/main/java/me/hhjeong/springbootcms/account/dto/CreateAccountRequest.java
+++ src/main/java/me/hhjeong/springbootcms/account/dto/CreateAccountRequest.java
@@ -4,15 +4,19 @@ import javax.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import me.hhjeong.springbootcms.account.domain.AccountId;
+import me.hhjeong.springbootcms.account.domain.AccountPassword;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateAccountRequest {
 
+    @AccountId
     @NotEmpty
     private String username;
 
+    @AccountPassword
     @NotEmpty
     private String password;
 }
