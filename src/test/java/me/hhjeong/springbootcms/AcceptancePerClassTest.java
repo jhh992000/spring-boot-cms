13102a3acbf63cd88754edceddb4809f5b9c58fd
HoonHee Jeong <jhh992000@gmail.com>
2021-10-18 18:32:56 +0900

    feat: 테스트 상위클래스에서 인가 프로세스 초기화

--- src/test/java/me/hhjeong/springbootcms/AcceptancePerClassTest.java
+++ src/test/java/me/hhjeong/springbootcms/AcceptancePerClassTest.java
@@ -1,16 +1,22 @@
 package me.hhjeong.springbootcms;
 
 import io.restassured.RestAssured;
+import me.hhjeong.springbootcms.common.security.metadatasource.UrlFilterInvocationSecurityMetadataSource;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class AcceptancePerClassTest extends AcceptanceTest {
+
+    @Autowired
+    private UrlFilterInvocationSecurityMetadataSource urlFilterInvocationSecurityMetadataSource;
 
     @BeforeAll
     public void init() {
         RestAssured.port = port;
         databaseCleanup.execute();
+        urlFilterInvocationSecurityMetadataSource.reload(); //spring security 인가 프로세스 초기화
     }
 
 }
