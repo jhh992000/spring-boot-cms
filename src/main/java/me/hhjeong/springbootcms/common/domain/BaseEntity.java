1af5393b6ebda0885152d65c88e803ac735e2f82
HoonHee Jeong <jhh992000@gmail.com>
2021-10-21 16:19:44 +0900

    feat: @ToString 애노테이션 추가

--- src/main/java/me/hhjeong/springbootcms/common/domain/BaseEntity.java
+++ src/main/java/me/hhjeong/springbootcms/common/domain/BaseEntity.java
@@ -3,12 +3,14 @@ package me.hhjeong.springbootcms.common.domain;
 import java.time.LocalDateTime;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@ToString
 public class BaseEntity {
 
     @CreatedDate
