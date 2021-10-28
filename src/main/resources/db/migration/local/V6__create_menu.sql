245dc48aeb510cc3d7f678a0514604f130dff0d2
HoonHee Jeong <jhh992000@gmail.com>
2021-10-28 13:35:49 +0900

    feat: menu ddl sql 추가

---
+++ src/main/resources/db/migration/local/V6__create_menu.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `menu` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `site_id` bigint(20) NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `upper_menu_id` bigint(20) NOT NULL,
+    `depth` bigint(20) NOT NULL,
+    `sort_no` bigint(20) NOT NULL,
+    `description` varchar(255) DEFAULT NULL,
+    `type` varchar(255) NOT NULL,
+    `link_url` varchar(255) DEFAULT NULL,
+    `open_type` varchar(255) NOT NULL,
+    `hide` bit(1) NOT NULL,
+    `enable` bit(1) NOT NULL,
+    `content_id` bigint(20) DEFAULT NULL,
+    `board_id` bigint(20) DEFAULT NULL,
+    `program_id` varchar(255) DEFAULT NULL,
+    `created_date` datetime DEFAULT NULL,
+    `modified_date` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_menu_site` (`site_id`),
+CONSTRAINT `fk_menu_site` FOREIGN KEY (`site_id`) REFERENCES `site` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
