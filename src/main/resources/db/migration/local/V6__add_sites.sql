67048be0e3d9bccdf1d813f82c83ab9b0d7defbe
HoonHee Jeong <jhh992000@gmail.com>
2021-11-02 03:25:35 +0900

    feat: 기본 사이트 데이터 추가 dml 추가

---
+++ src/main/resources/db/migration/local/V6__add_sites.sql
@@ -0,0 +1,8 @@
+INSERT INTO site(name, enabled, alias, use_login_lock, count_of_login_fail, created_datetime, modified_datetime)
+VALUES('국문홈페이지', 1, 'ko', 1, 5, now(), null);
+
+INSERT INTO site(name, enabled, alias, use_login_lock, count_of_login_fail, created_datetime, modified_datetime)
+VALUES('영문홈페이지', 1, 'en', 1, 5, now(), null);
+
+INSERT INTO site(name, enabled, alias, use_login_lock, count_of_login_fail, created_datetime, modified_datetime)
+VALUES('관리자사이트', 1, 'admin', 1, 5, now(), null);
