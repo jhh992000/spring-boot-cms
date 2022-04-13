b87da950185f8a519a9edfa476c79d7629763612
HoonHee Jeong <jhh992000@gmail.com>
2022-04-14 01:35:52 +0900

    feat: eslint 설정파일 추가

---
+++ frontend/frontend-admin/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jquery: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    'no-shadow': 'off',
+    semi: 0,
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'vue/attributes-order': 'off',
+    'vue/name-property-casing': 'off',
+    'vue/no-v-html': 'off',
+    'vue/comment-directive': 'off',
+    'dot-notation': 'error',
+    'linebreak-style': 'off',
+    'no-console': process.env.NODE_ENV === 'release' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'release' ? 'warn' : 'off',
+    // 'prettier/prettier': ['error', { endOfLine: 'crlf' }],
+  },
+};
